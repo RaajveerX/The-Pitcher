51132e133de513a99843e5f1878215d347dd2280
Raajveer <63507532+RaajveerX@users.noreply.github.com>
2022-02-24 16:30:30 -0500

    Create items.h

---
+++ items.h
@@ -0,0 +1,20 @@
+#include <stdbool.h>
+
+//namoona means a characters in a  drama in hindi
+struct Avatar{
+   char* name;
+   struct Avatar* next;
+};
+
+
+//Item node ( used to implement a linked list of items ) 
+struct Item{
+   char* name;
+   struct Item* next;
+};
+
+
+
+
+
+
